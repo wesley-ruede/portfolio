993d9a73ff6dad075a123149f54733c4db054d68
Wes <wesley.k.r1993@gmail.com>
2019-05-26 19:34:03 -0400

    Blog app now works

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+	title = models.CharField(max_length=50, default='Enter the Title')
+	pub_date = models.DateTimeField(null=True)
+	body = models.TextField(max_length=500, default='Enter your blog post')
+	image = models.ImageField(upload_to='images/')
